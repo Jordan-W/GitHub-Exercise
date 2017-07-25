363d45c46f7e4bfe4d96bb4bbd291c4c5aedb260
Jordan Wolf <j_wolf23@yahoo.com>
2017-07-25 15:52:21 -0500

    First COmmit

--- GitHub Excercise/ViewController.swift
+++ GitHub Excercise/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var firstName = "Billy"
+        var lastName = "Maes"
+        print("\(firstName) \(lastName)")
     }
 
     override func didReceiveMemoryWarning() {
